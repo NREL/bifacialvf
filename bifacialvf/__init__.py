f330fcc392d1ea917bcbce95810820ef46a14f85
Thomas Capelle <thomascapelle@gmail.com>
2019-09-20 17:14:04 +0200

    added import of simulate_inner

--- bifacialvf/__init__.py
+++ bifacialvf/__init__.py
@@ -1,6 +1,6 @@
 # ensure python3 compatible division and printing
 from __future__ import division, print_function, absolute_import
-from .bifacialvf import simulate  # main program
+from .bifacialvf import simulate, simulate_inner  # main program
 from .vf import getBackSurfaceIrradiances, getFrontSurfaceIrradiances, getGroundShadeFactors  # main subroutines
 from .vf import getSkyConfigurationFactors, trackingBFvaluescalculator, rowSpacing # helper functions
 from .sun import hrSolarPos, perezComp, solarPos, sunIncident # solar position and value

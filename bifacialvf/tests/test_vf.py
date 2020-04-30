416037b588e4112d8203133bacae6debe0471955
cdeline <chris.deline@nrel.gov>
2020-04-29 22:54:33 -0600

    first, last, single getskyconfiguration tests. 66% coverage

--- bifacialvf/tests/test_vf.py
+++ bifacialvf/tests/test_vf.py
@@ -8,7 +8,8 @@ from bifacialvf.vf import getSkyConfigurationFactors
 from bifacialvf.tests import (
     SKY_BETA160_C05_D1, SKY_BETA20_C05_D1, SKY_BETA20_C0_D1, SKY_BETA160_C0_D1,
     SKY_BETA160_C1_D1, SKY_BETA20_C1_D1, SKY_BETA20_C1_D0, SKY_BETA160_C1_D0,
-    SKY_BETA160_C05_D0, SKY_BETA20_C05_D0)
+    SKY_BETA160_C05_D0, SKY_BETA20_C05_D0, SKY_20_1_1_first, SKY_20_1_1_last,
+    SKY_20_1_1_single)
 
 
 @pytest.mark.parametrize('beta, C, D, expected',
@@ -23,3 +24,13 @@ def test_getSkyConfigurationFactors(beta, C, D, expected):
     """
     assert np.allclose(
         getSkyConfigurationFactors("interior", beta, C, D), expected)
+
+@pytest.mark.parametrize('rowtype, expected',
+    [('first', SKY_20_1_1_first), ('last', SKY_20_1_1_last),
+     ('single', SKY_20_1_1_single)])
+def test_getSkyConfigurationsRowtype(rowtype, expected):
+    """
+    Benchmark against to the master branch on 2018-08-20 at 91e785d.
+    """
+    assert np.allclose(
+        getSkyConfigurationFactors(rowtype, beta=20, C=1, D=1), expected)
